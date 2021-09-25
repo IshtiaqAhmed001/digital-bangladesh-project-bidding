667a07d62ca059d3859186ebec8d26f6f924e4e9
IshtiaqAhmed001 <ishtiaq.wasi@gmail.com>
2021-09-26 01:05:49 +0600

    first commit

---
+++ src/components/Header/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Header.css';
+const Header = () => {
+    return (
+        <div className="header-container">
+            <h1>this is my header</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet cupiditate voluptates maxime eius! Et dolores deserunt fugiat, doloremque eos facilis natus quos unde temporibus facere dolorum, libero rem officia asperiores.</p>
+            <h3>Total Budget: 200 Million</h3>
+        </div>
+    );
+};
+
+export default Header;
